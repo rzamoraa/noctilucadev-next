9c8ae5ec0079eb90005b98dec215241c06bfe35d
rzamoraa <rzamoraamestica@gmail.com>
2024-05-14 22:28:48 -0400

    analithic add3

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 import Image from "next/image";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <div className="flex flex-col items-center">
+    <Analytics/> 
       <h1 className="text-4xl font-bold">Hello, World!</h1>
       <p className="text-lg text-gray-500">Welcome to your new app!</p>
     </div>
-    
+
     </main>
   );
 }
